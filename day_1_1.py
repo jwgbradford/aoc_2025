f59c4c1114d1ed61b13cd7892f4b957d52c487f8
John Bradford <jwgbradford@gmail.com>
2025-12-06 08:34:44 +0000

    test working

--- day_1_1.py
+++ day_1_1.py
@@ -1,4 +1,24 @@
 with open('day_1_test.txt') as my_file:
-    data = my_file.read().splitlines()
+    data : list[str] = my_file.read().splitlines()
 
-print(data)
+password : int = 0
+dial : int = 50
+
+for line in data:
+    direction : str = line[0]
+    steps : int = int(line[1:])
+
+    if direction == 'R':
+        dial += steps
+    elif direction == 'L':
+        dial -= steps
+
+    if dial > 99:
+        dial = dial - 100
+    elif dial < 0:
+        dial = dial + 100
+
+    if dial == 0:
+        password += 1
+
+print(password)
