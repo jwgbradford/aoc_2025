2112c97511622593ec68c3521f978d96ca902b97
jwgbradford <jwgbradford@gmail.com>
2025-12-06 10:44:29 +0000

    lost concentration

--- day_2.py
+++ day_2.py
@@ -2,9 +2,11 @@ with open('day_2_test.txt') as my_file:
     data : list[str] = my_file.read().split(',')
 
 invalid_id_total : int = 0
+print(data)
 
 for id_range in data:
     id_lower, id_upper = id_range.split('-')
-    for id in range(int(id_lower), int(id_upper)):
+    print(id_lower, id_upper)
+    for id in range(int(id_lower), int(id_upper)+1):
         if len(str(id)) % 2 == 0:
             pass
