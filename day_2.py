5caf93bf286619af8fe4f5c398ddb8be7945307c
jwgbradford <jwgbradford@gmail.com>
2025-12-06 10:32:58 +0000

    part working

--- day_2.py
+++ day_2.py
@@ -1,4 +1,10 @@
 with open('day_2_test.txt') as my_file:
     data : list[str] = my_file.read().split(',')
 
-print(data)
+invalid_id_total : int = 0
+
+for id_range in data:
+    id_lower, id_upper = id_range.split('-')
+    for id in range(int(id_lower), int(id_upper)):
+        if len(str(id)) % 2 == 0:
+            pass
