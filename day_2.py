57128ee25ba40d324da9bcca3b16fda16b116878
= <=>
2025-12-06 13:54:35 +0000

    test works

--- day_2.py
+++ day_2.py
@@ -2,11 +2,13 @@ with open('day_2_test.txt') as my_file:
     data : list[str] = my_file.read().split(',')
 
 invalid_id_total : int = 0
-print(data)
 
 for id_range in data:
     id_lower, id_upper = id_range.split('-')
-    print(id_lower, id_upper)
     for id in range(int(id_lower), int(id_upper)+1):
-        if len(str(id)) % 2 == 0:
-            pass
+        id_str = str(id)
+        if len(id_str) % 2 == 0:
+            if id_str[:len(id_str)//2] == id_str[len(id_str)//2:]:
+                invalid_id_total += 1
+                print(f"Invalid ID found: {id_str}")
+print(invalid_id_total)
