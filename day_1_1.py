888e5062aea5c426f852d964cf7854482647ff12
John Bradford <jwgbradford@gmail.com>
2025-12-06 08:55:52 +0000

    need to handle looping better

--- day_1_1.py
+++ day_1_1.py
@@ -1,4 +1,5 @@
-with open('day_1_1_input.txt') as my_file:
+from math import modf
+with open('day_1_test.txt') as my_file:
     data : list[str] = my_file.read().splitlines()
 
 password : int = 0
@@ -8,6 +9,7 @@ for line in data:
     direction : str = line[0]
     steps : int = int(line[1:])
 
+    password += modf(steps / 100)[1] # add the number of full rotations to the password
     steps = steps % 100 # since the dial wraps around every 100
 
     if direction == 'R':
@@ -15,12 +17,14 @@ for line in data:
     elif direction == 'L':
         dial -= steps
 
+    # need to catch the case where we wrap around the dial, but not count if we start on 0
     if dial > 99:
         dial = dial - 100
     elif dial < 0:
         dial = dial + 100
-
+    
     if dial == 0:
         password += 1
+    print(f"Direction: {direction}, Steps: {steps}, Dial: {dial}, Current Password: {password}")
 
 print(password)
