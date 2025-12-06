49a86b9c1d70072857dd8639d080f0f880270efa
jwgbradford <jwgbradford@gmail.com>
2025-12-06 10:12:06 +0000

    day 1 solved

--- day_1_1.py
+++ day_1_1.py
@@ -1,15 +1,15 @@
-from math import modf
-with open('day_1_test.txt') as my_file:
+with open('day_1_input.txt') as my_file:
     data : list[str] = my_file.read().splitlines()
 
 password : int = 0
 dial : int = 50
+dial_is_zero : bool = False
 
 for line in data:
     direction : str = line[0]
     steps : int = int(line[1:])
 
-    password += modf(steps / 100)[1] # add the number of full rotations to the password
+    password += steps // 100 # add the number of full rotations to the password
     steps = steps % 100 # since the dial wraps around every 100
 
     if direction == 'R':
@@ -17,14 +17,20 @@ for line in data:
     elif direction == 'L':
         dial -= steps
 
-    # need to catch the case where we wrap around the dial, but not count if we start on 0
     if dial > 99:
         dial = dial - 100
+        if not dial_is_zero and dial != 0:
+            password += 1
     elif dial < 0:
         dial = dial + 100
-    
+        if not dial_is_zero:
+            password += 1
+
     if dial == 0:
         password += 1
-    print(f"Direction: {direction}, Steps: {steps}, Dial: {dial}, Current Password: {password}")
+        dial_is_zero = True
+    else:
+        dial_is_zero = False
+    #print(f"Direction: {direction}, Steps: {steps}, Dial: {dial}, Current Password: {password}")
 
 print(password)
