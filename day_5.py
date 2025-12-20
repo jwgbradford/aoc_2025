3a578fc0f66d1515f71ffeebe0a4ec220919d099
jwgbradford <jwgbradford@gmail.com>
2025-12-20 14:49:52 +0000

    multiple overlaps

--- day_5.py
+++ day_5.py
@@ -1,4 +1,7 @@
-with open('day_5_input.txt') as file:
+def check_overlap(range1, range2) -> bool:
+        return max(range1[0],range2[-1]) < min(range1[-1],range2[0])
+
+with open('day_5_test.txt') as file:
     data = file.read().split()
 
 #print(data)
@@ -8,10 +11,38 @@ available = []
 
 for entry in data:
     if '-' in entry:
-        ingredients.append(entry.split('-'))
+        lower, upper = entry.split('-')
+        ingredients.append([int(lower), int(upper)])
     else:
         available.append(int(entry))
 
+print(ingredients)
+range_list = []
+
+for test_range in ingredients:
+    if len(range_list) == 0:
+        range_list.append(test_range)
+    else:
+        no_overlap = True
+        for overlap in range_list:
+            if max(test_range[0],overlap[0]) < min(test_range[-1],overlap[-1]):
+                if overlap[0] < test_range[0]:
+                    test_range[0] = overlap[0]
+                if overlap[-1] > test_range[-1]:
+                    test_range[-1] = overlap[-1]
+                no_overlap = False
+                break
+        if no_overlap:
+            range_list.append(test_range)
+
+print(range_list)
+
+range_counter = 0
+for each_range in range_list:
+    range_counter += each_range[1]-each_range[0] + 1
+
+print(range_counter)
+'''
 fresh = 0
 
 for item in available:
@@ -21,3 +52,4 @@ for item in available:
             break
 
 print(fresh)
+'''
